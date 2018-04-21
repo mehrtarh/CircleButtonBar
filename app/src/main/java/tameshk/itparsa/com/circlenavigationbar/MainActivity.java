60caf199553d99bf4c15ef4fec659c7999b6d1da
mehran jafari <mehrtarh@yahoo.com>
2018-04-21 14:53:58 +0430

    add project

---
+++ app/src/main/java/tameshk/itparsa/com/circlenavigationbar/MainActivity.java
@@ -0,0 +1,49 @@
+package tameshk.itparsa.com.circlenavigationbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.itparsa.circlenavigation.CircleItem;
+import com.itparsa.circlenavigation.CircleNavigationView;
+import com.itparsa.circlenavigation.CircleOnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CircleNavigationView mCircleNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCircleNavigationView = findViewById(R.id.navigation);
+        mCircleNavigationView.initWithSaveInstanceState(savedInstanceState);
+        mCircleNavigationView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        mCircleNavigationView.addCircleItem(new CircleItem("setting", R.drawable.ic_settings));
+        mCircleNavigationView.addCircleItem(new CircleItem("profile", R.drawable.ic_person));
+        mCircleNavigationView.addCircleItem(new CircleItem("chat", R.drawable.ic_chat));
+        mCircleNavigationView.addCircleItem(new CircleItem("share", R.drawable.ic_share));
+
+        mCircleNavigationView.setCircleOnClickListener(new CircleOnClickListener() {
+            @Override
+            public void onCentreButtonClick() {
+
+            }
+
+            @Override
+            public void onItemClick(int itemIndex, String itemName) {
+
+            }
+
+            @Override
+            public void onItemReselected(int itemIndex, String itemName) {
+
+            }
+        });
+
+//        mCircleNavigationView.setCentreButtonIcon(R.drawable.ic_home);
+//        mCircleNavigationView.setActiveCentreButtonIconColor(ContextCompat.getColor(MainActivity.this, android.R.color.white));
+//        mCircleNavigationView.setActiveCentreButtonBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.colorAccent));
+    }
+}
